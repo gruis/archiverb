1ed0cffaf634670aff998e33a1662eb7e20d0bd2
Caleb Crane <github@simulacre.org>
2012-01-23 00:25:29 +0900

    tar spec pases, but added pending spec for links

--- spec/tar_spec.rb
+++ spec/tar_spec.rb
@@ -17,7 +17,7 @@ describe Archiver::Tar do
     tar["data/henryIV.txt"].stat.readlink.should == "heneryIV.txt"
   end # should correctly unarchive text data
 
-  it "should correct tar text data" do
+  it "should correctly tar text data" do
     Dir.chdir(File.dirname(__FILE__)) do
       Archiver::Tar.new.tap do |archive|
         archive.add("data/heneryIV.txt")
@@ -25,11 +25,15 @@ describe Archiver::Tar do
         archive.add("data/henryIV.txt")
         archive.count.should == 3
         archive.files.should_not be_empty
-        archive.files.map(&:name).should == ['data/heneryIV.txt', 'data/heneryIV-westmoreland.txt', 'data/henryIV.txt']
+        archive.names == ['data/heneryIV.txt', 'data/heneryIV-westmoreland.txt', 'data/henryIV.txt']
         archive.write do |raw|
           #Digest::MD5.hexdigest(raw).should == "428500d94224b52844b0499af60da8d4"
         end # raw
       end # archive
     end
-  end # should correct tar text data
+  end # should correctly tar text data
+
+  it "should correctly tar links" do
+    pending
+  end # should correctly tar links
 end # Archiver::Tar
